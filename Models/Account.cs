4d8598ab0296ec27566b4cbddca62aef61c967fb
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-10 20:08:12 +0200

    Create Account

---
+++ Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using FischotterFinanceManager.Entities;
+
+namespace FischotterFinanceManager.Models;
+
+public class Account(Guid id, string title, string description) : NamedEntity(id, title, description)
+{
+    #region properties
+
+    #region navigationProperties
+
+    public List<Payment> Payments { get; private init; } = [];
+
+    #endregion navigationProperties
+
+    #endregion properties
+}
