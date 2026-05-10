27a955f601b2c3bdc51593aaaab393abf705e9de
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-10 20:08:25 +0200

    Create Payment.

---
+++ Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿using FischotterFinanceManager.Entities;
+
+namespace FischotterFinanceManager.Models;
+
+public class Payment(Guid id, string title, string description, decimal amount) : NamedEntity(id, title, description)
+{
+    #region properties
+
+    public decimal Amount { get; set; } = amount;
+
+    #endregion properties
+}
