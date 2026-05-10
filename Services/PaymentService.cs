b54c063f48e4392a7f6d72e1fe849266a1433068
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-10 20:22:49 +0200

    Create Payment Service.

---
+++ Services/PaymentService.cs
@@ -0,0 +1,28 @@
+﻿using FischotterFinanceManager.Models;
+
+namespace FischotterFinanceManager.Services;
+
+public class PaymentService
+{
+    #region properties
+
+    public List<Payment> Payments { get; private init; } = [];
+
+    #endregion properties
+
+    #region methods
+
+    public Payment CreatePayment(Guid id, string title, string description, decimal amount)
+    {
+        Payment payment = new(id, title, description, amount);
+        Payments.Add(payment);
+        return payment;
+    }
+
+    public void DeletePayment(Payment payment)
+    {
+        Payments.Remove(payment);
+    } 
+
+    #endregion methods
+}
