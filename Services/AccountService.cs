f8571f7148cfba4fce811fcef8d42a44d31d4a61
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-10 20:26:10 +0200

    Create Account Service.

---
+++ Services/AccountService.cs
@@ -0,0 +1,28 @@
+﻿using FischotterFinanceManager.Models;
+
+namespace FischotterFinanceManager.Services;
+
+public class AccountService
+{
+    #region properties
+
+    public List<Account> Accounts { get; private init; } = [];
+
+    #endregion properties
+
+    #region methods
+
+    public Account CreateAccount(Guid id, string title, string description)
+    {
+        Account account = new(id, title, description);
+        Accounts.Add(account);
+        return account;
+    }
+
+    public void DeleteAccount(Account account)
+    {
+        Accounts.Remove(account);
+    }
+
+    #endregion methods
+}
