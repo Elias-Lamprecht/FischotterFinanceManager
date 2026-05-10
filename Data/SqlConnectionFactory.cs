45d3060b4797bba75de123b4e6f0d5c1890d5941
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-10 20:34:51 +0200

    Create SQL Connection Factory.

---
+++ Data/SqlConnectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using FischotterFinanceManager.Envs;
+
+namespace TaskTracker.Data;
+
+#region Interface
+
+public interface IDbConnectionFactory
+{
+    Task<SqlConnection> CreateOpenConnectionAsync();
+}
+
+#endregion Interface
+
+public class SqlConnectionFactory(string connectionString = Envs.ConnectionString) : IDbConnectionFactory
+{
+    #region properties
+
+    private readonly string _connectionString = connectionString;
+
+    #endregion properties
+
+    #region methods
+
+    public async Task<SqlConnection> CreateOpenConnectionAsync()
+    {
+        SqlConnection sqlConnection = new(_connectionString);
+        await sqlConnection.OpenAsync();
+        return sqlConnection;
+    }
+
+    #endregion methods
+}
