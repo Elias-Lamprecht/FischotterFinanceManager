cef3098e43c77bb0aa19ff27b8934a4f92b2ea9f
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-13 21:01:18 +0200

    Create Menu & UI/Component Folder

---
+++ UI/Components/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿namespace FischotterFinanceManager.UI.Components;
+
+public class MenuItem(string label, Action action, string category)
+{
+    #region properties
+
+    public string Label { get; private set; } = label;
+    public Action Action { get; private set; } = action;
+    public string Category { get; private set; } = category;
+
+    #endregion properties
+}
