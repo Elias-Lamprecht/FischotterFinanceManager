a036d69fc69d89221a2ffe68b7155d97ebfdecb0
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-14 07:44:41 +0200

    Create MenuItem

--- UI/Components/MenuItem.cs
+++ UI/Components/MenuItem.cs
@@ -1,12 +1,11 @@
 ﻿namespace FischotterFinanceManager.UI.Components;
 
-public class MenuItem(string label, Action action, string category)
+public class MenuItem(string label, Action? action)
 {
     #region properties
 
     public string Label { get; private set; } = label;
-    public Action Action { get; private set; } = action;
-    public string Category { get; private set; } = category;
+    public Action? Action { get; private set; } = action;
 
     #endregion properties
 }
