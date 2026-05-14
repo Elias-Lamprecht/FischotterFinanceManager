f8f1b2a78722b26426f143b8c4774dafd33285c7
EliasLamprecht <eliaslamprecht3@gmail.com>
2026-05-14 07:45:15 +0200

    Create basic Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Fischotter Finance Manager! :D");
+﻿using FischotterFinanceManager.UI.Components;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            _menu.DisplayMenu();
+        }
+    }
+
+    #region fields
+
+    private static readonly Menu _menu = new();
+
+    #endregion fields
+}
